5fd68116fdc145b31e3c33abd76ff7b21ee0c06b
kingsleychino <kingsleychinono@gmail.com>
2025-10-06 22:34:17 +0100

    Update iam_role.tf

--- iam_role.tf
+++ iam_role.tf
@@ -11,9 +11,9 @@ resource "aws_iam_role_policy_attachment" "ecs_exec_role_policy" {
 
 resource "aws_cloudwatch_log_group" "ecs_logs" {
   name = "/ecs/java-app"
+  retention_in_days = 7
 
   lifecycle {
     prevent_destroy = true
-    ignore_changes  = [tags]
   }
 }
